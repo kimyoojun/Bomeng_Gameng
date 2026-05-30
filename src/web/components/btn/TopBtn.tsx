13b4ca57e158cbdf21498062671a01405c687114
kimyoojun <anano3134@gmail.com>
2026-05-30 16:29:11 +0900

    feat: button component hover 기능과 backgroundColor props 추가
    onMouseEnter, onMouseLeace를 사용하여 button 컴포넌트 위 마우스 상태를 useState에 저장 후 hover 적용 여부 설정 및 backgroundColor props 기본값 설정 후 추가

--- src/web/components/btn/TopBtn.tsx
+++ src/web/components/btn/TopBtn.tsx
@@ -1,19 +1,23 @@
 import "/src/web/css/btn.css";
+import { useState } from "react";
 
 type TopBtnProps = {
         btnName: string
         btnImg: string
         altName: string
+        bgColor?: string
+        hoverColor?: string
     }
 
-
-
 function TopBtn (props:TopBtnProps) {
+    const [ isHover, setIsHover ] = useState(false)
 
-    
-    
     return (
-        <a className="tBtn" style={{ padding: "8px", borderRadius: "6px", fontSize: "15px", fontWeight: "500", color: "black", display: "flex", alignItems: "center" }}>
+        <a 
+        className="tBtn" 
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)} 
+        style={{ backgroundColor: isHover ? props.hoverColor ?? "#f0f0f0" : props.bgColor ?? "white", padding: "8px", borderRadius: "6px", fontSize: "15px", fontWeight: "500", color: "black", display: "flex", alignItems: "center" }}>
             <img src={props.btnImg} style={{ marginRight: "15px" }} alt={props.altName}/>
             {props.btnName}
         </a>
