0160bcb26829b12c4407621308efe82cbe6d532f
kimyoojun <anano3134@gmail.com>
2026-05-30 18:52:32 +0900

    feat: main화면 body 부분 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import TopBar from './web/components/TopBar.tsx'
+import MainBody from './web/components/MainBody.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TopBar/>
-    {/* <App /> */}
+    <div>
+      <TopBar/>
+      <MainBody/>
+      {/* <App /> */}
+    </div>
   </StrictMode>,
 )
