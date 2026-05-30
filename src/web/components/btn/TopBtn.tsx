ee75a9f18cd0ec57bf656725ec21080f3be4a756
kimyoojun <anano3134@gmail.com>
2026-05-30 18:50:27 +0900

    feat: TopBtn component 색상반전, 폰트 사이즈, border 여부 선택 기능 추가

--- src/web/components/btn/TopBtn.tsx
+++ src/web/components/btn/TopBtn.tsx
@@ -7,6 +7,9 @@ type TopBtnProps = {
         altName: string
         bgColor?: string
         hoverColor?: string
+        isInvert?: string
+        fontSize?: string
+        isBorder?: string
     }
 
 function TopBtn (props:TopBtnProps) {
@@ -17,7 +20,7 @@ function TopBtn (props:TopBtnProps) {
         className="tBtn" 
         onMouseEnter={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)} 
-        style={{ backgroundColor: isHover ? props.hoverColor ?? "#f0f0f0" : props.bgColor ?? "white", padding: "8px", borderRadius: "6px", fontSize: "15px", fontWeight: "500", color: "black", display: "flex", alignItems: "center" }}>
+        style={{ backgroundColor: isHover ? props.hoverColor ?? "#f0f0f0" : props.bgColor ?? "white", padding: "8px", borderRadius: "6px", fontSize: props.fontSize ?? "15px", fontWeight: "500", color: "black", display: "flex", alignItems: "center", filter: props.isInvert, border: props.isBorder ?? "none" }}>
             <img src={props.btnImg} style={{ marginRight: "15px" }} alt={props.altName}/>
             {props.btnName}
         </a>
