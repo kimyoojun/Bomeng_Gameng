a9bfdddce011dcab70e2c95aa27c7995ed6eda90
kimyoojun <anano3134@gmail.com>
2026-05-31 16:44:31 +0900

    feat: Home page 추가

---
+++ src/web/Home.tsx
@@ -0,0 +1,9 @@
+import MainBody from "./components/MainBody"
+
+export default function Home() {
+    return(
+        <div style={{ width: "100%", height: "100%" }}>
+            <MainBody/>
+        </div>
+    )
+}
