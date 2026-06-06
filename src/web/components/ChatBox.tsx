a725b1ded2c5c7fa32533f102e8b86238c42b307
kimyoojun <anano3134@gmail.com>
2026-06-06 14:14:52 +0900

    chore: chatBox 간에 상하 여백 추가 및  chatText 원본 포맷 유지

--- src/web/components/ChatBox.tsx
+++ src/web/components/ChatBox.tsx
@@ -4,12 +4,12 @@ type ChatBoxProps = {
 
 function ChatBox(props: ChatBoxProps) {
     return(
-        <div style={{ width: "100%", display: "flex", alignItems: "start", justifyContent: "start", gap: "12px" }}>
+        <div style={{ width: "100%", display: "flex", alignItems: "start", justifyContent: "start", gap: "12px", marginBottom: "10px" }}>
             <div style={{ width: "32px", height: "32px", borderRadius: "9999px", backgroundColor: "#dbeafe", display: "flex", justifyContent: "center", alignItems: "center" }}>
                 <img src="/src/assets/chatbot.png" alt="챗봇 아이콘" style={{ width: "24px", height: "24px", filter: "brightness(0) saturate(100%) invert(44%) sepia(73%) saturate(7210%) hue-rotate(217deg) brightness(97%) contrast(103%)" }}/>
             </div>
             <div style={{ backgroundColor: "#f6f3f4", borderRadius: "16px", padding: "12px 16px" }}>
-                <p style={{ textAlign: "start", color: "black", fontSize: "16px" }}>
+                <p style={{ textAlign: "start", color: "black", fontSize: "16px", whiteSpace: "pre-wrap" }}>
                     {props.chatText}
                 </p>
             </div>
